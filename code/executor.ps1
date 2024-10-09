43778351fc7b8751fc5db6ea7235f63f36bb737f
Leime <67313990+tjwhang@users.noreply.github.com>
2024-10-09 19:20:26 +0900

    Update executor.ps1

--- code/executor.ps1
+++ code/executor.ps1
@@ -1,6 +1,16 @@
 # PowerShell script to download and execute a .cmd script from GitHub
+@echo off
 
 echo "Downloading script at env:temp"
+
+where node >nul 2>nul
+if %ERRORLEVEL% equ 0 (
+    echo Node.js is installed.
+    node -v
+) else (
+    echo Node.js is not installed.
+)
+
 
 # URL to the raw .cmd script on GitHub
 $scriptUrl = "https://raw.githubusercontent.com/tjwhang/IRM-APITest/refs/heads/main/code/main.cmd"
